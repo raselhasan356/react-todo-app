d3a73d07fce4ab5a48cb74d1c80ddb08f3794177
Rasel Hasan <rasel.hasan@vivacomsolutions.com>
2023-08-16 16:21:01 +0600

    implementation done

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -4,5 +4,37 @@ import { useTodo } from '../context'
 import { Input } from './Input'
 
 export const AddTodo = () => {
-  return <div>Add To Do</div>
+  const [input, setInput] = useState<string>('')
+  const { addTodo } = useTodo()
+
+  const handleSubmission = (e: React.FormEvent) => {
+    e.preventDefault()
+    if (input.trim() !== '') {
+      addTodo(input)
+      setInput('')
+      toast.success('Todo added successfully!')
+    } else {
+      toast.error('Todo field cannot be empty!')
+    }
+    console.log('form has been submitted.')
+  }
+
+  return (
+    <form onSubmit={handleSubmission}>
+      <div className="flex items-center w-full max-w-lg gap-2 p-5 m-auto">
+        <Input
+          value={input}
+          onChange={e => setInput(e.target.value)}
+          type="text"
+          placeholder="start typing ..."
+        />
+        <button
+          type="submit"
+          className="px-5 py-2 text-sm font-normal text-blue-300 bg-blue-900 border-2 border-blue-900 active:scale-95 rounded-xl"
+        >
+          Submit
+        </button>
+      </div>
+    </form>
+  )
 }
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,8 +1,24 @@
-import { InputHTMLAttributes, forwardRef } from 'react'
+import { InputHTMLAttributes, useEffect, useRef, forwardRef } from 'react'
 import cn from 'classnames'
 
-import React from 'react'
+export const Input = forwardRef<
+  HTMLInputElement,
+  InputHTMLAttributes<HTMLInputElement>
+>(({ className, ...rest }, ref) => {
+  const inputRef = useRef<HTMLInputElement>(null)
 
-export const Input = () => {
-  return <div>Input</div>
-}
+  useEffect(() => {
+    inputRef.current?.focus()
+  }, [])
+
+  return (
+    <input
+      {...rest}
+      ref={inputRef}
+      className={cn(
+        'w-full px-5 py-2 bg-transparent border-2 outline-none border-zinc-600 rounded-xl placeholder:text-zinc-500 focus:border-white',
+        className,
+      )}
+    />
+  )
+})
